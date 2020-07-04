cf45c43a8a6f7e164b0528c49885a2d9c523ce5c
unknown <fegoworks@hotmail.com>
2020-07-04 13:12:07 +0100

    fix(models): tweak migrations and models

---
+++ src/migrations/20200703104509-create-transaction.js
@@ -0,0 +1,22 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Transactions', {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Transactions')
+};
