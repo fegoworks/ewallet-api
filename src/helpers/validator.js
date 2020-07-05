06ba36a2001584a314c30ef8fa9fc9a53cc460e6
unknown <fegoworks@hotmail.com>
2020-07-05 22:29:04 +0100

    feat(validation): validate inputs to wallet routes

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -16,39 +16,41 @@ const validator = {
 
   schemas: {
     authSchema: Joi.object().keys({
-      firstName: Joi.string()
-        .regex(/^[a-zA-Z]*$/)
-        .required()
-        .trim()
+      firstName: Joi.string().regex(/^[a-zA-Z]*$/).required().trim()
         .lowercase()
         .error(new Error('First Name is required')),
-      lastName: Joi.string()
-        .regex(/^[a-zA-Z\\-]*$/)
-        .required()
-        .trim()
+      lastName: Joi.string().regex(/^[a-zA-Z\\-]*$/).required().trim()
         .lowercase()
         .error(new Error('Last Name is required')),
-      email: Joi.string()
-        .email()
-        .required()
-        .trim()
+      email: Joi.string().email().required().trim()
         .lowercase()
         .error(new Error('A valid email address is required')),
-      password: Joi.string()
-        .required()
-        .error(new Error('Password is required')),
+      password: Joi.string().required().error(new Error('Password is required')),
     }),
+
     authLoginSchema: Joi.object().keys({
-      email: Joi.string()
-        .regex(/\S+@\S+\.\S+/)
-        .required()
-        .trim()
+      email: Joi.string().regex(/\S+@\S+\.\S+/).required().trim()
         .lowercase()
         .error(new Error('A valid email address is required')),
-      password: Joi.string()
-        .required()
-        .error(new Error('Password is required')),
+      password: Joi.string().required().error(new Error('Password is required')),
     }),
+
+    fundAccountSchema: Joi.object().keys({
+      amount: Joi.number().required().error(new Error('Please enter amount')),
+      cvv: Joi.number().integer().max(999).required()
+        .error(new Error('Enter a valid CVV')),
+      number: Joi.string().regex(/^[0-9]*$/).required().error(new Error('Enter a valid card number string')),
+      expiry_month: Joi.number().integer().min(1).max(12)
+        .required()
+        .error(new Error('Enter an expiry month')),
+      expiry_year: Joi.number().integer().max(2099).required()
+        .error(new Error('Enter an expiry year')),
+    }),
+
+    transferSchema: Joi.object().keys({
+      amount: Joi.number().required().error(new Error('Please enter amount')),
+      accountNumber: Joi.number().integer().required().error(new Error('Enter a valid account number')),
+    })
   },
 };
 
--- src/routes/wallet.route.js
+++ src/routes/wallet.route.js
@@ -4,15 +4,18 @@ import {
 } from '../middlewares/auth/auth.middleware';
 import FundingController from '../controllers/funding.controller';
 import WalletController from '../controllers/wallet.controller';
+import validate from '../helpers/validator';
 
 const router = express.Router();
 
 router.post('/fundAccount',
   verifyToken.verify,
+  validate.validateBody(validate.schemas.fundAccountSchema),
   FundingController.fundAccount);
 
 router.post('/wallets/transfer',
   verifyToken.verify,
+  validate.validateBody(validate.schemas.transferSchema),
   WalletController.transferToWallet);
 
 router.get('/wallets/:customerId',
